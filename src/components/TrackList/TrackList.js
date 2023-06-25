8955f47efedeb5202ae865372d1b2f321d6cd0d9
IamNatetrizton <taiwotrizton@gmail.com>
2023-06-25 23:21:57 +0100

    added search result folder

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./TrackList.css";
+
+import Track from "../Track/Track";
+
+class TrackList extends React.Component{
+    render(){
+        return {
+           div className="TrackList">
+           {this.props.track.map(track => {
+            
+            return{
+                <Track
+                track = {track}
+                key = {track.id}
+                onAdd = {this.props.onAdd}
+                isRemoveal = {this.props.isRemoval}
+                onRemove = {this.props.onRemove}
+                
+                
+                
+                
+                />
+            };
+            
+            
+         } ) }
+          </div>
+        };
+    }
+
+
+}
+
+export default TrackList;
